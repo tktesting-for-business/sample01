fac4bc3622167465621786faf3c90f7e81b00af3
tktesting-for-business <taka3chijp@gmail.com>
2025-01-26 09:14:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,32 @@
 
 import streamlit as st
 import base64
-import sys
 from PIL import Image
+from io import BytesIO
 
+# 画像ファイルのパス
+IMAGE_PATH = "aaa.jpeg" # ここにイメージファイルのパスを設定してください
 
 st.title("Embedding Dify app in Streamlit")
 
 # img = Image.open('aaa.jpg')
 # st.image(img)
 
+# 画像を読み込み
+try:
+  image = Image.open(IMAGE_PATH)
+except FileNotFoundError:
+    st.error(f"エラー：画像ファイル '{IMAGE_PATH}' が見つかりませんでした。")
+    st.stop()
 
-# 画像ファイルをbase64エンコードしてCSSに埋め込む
-def get_base64_of_image(image_path):
+# 画像をバイト列に変換
+buffered = BytesIO()
+image.save(buffered, format="JPEG")
+img_str = base64.b64encode(buffered.getvalue()).decode()
+img_url = f"data:image/jpeg;base64,{img_str}"
 
-    #try:
-    with open(image_path, "rb") as image_file:
-        encoded_string = "test"
-            # encoded_string = base64.b64encode(image_file.read()).decode()
-    #except FileNotFoundError as e:
-        sencoded_string = e
-        
-    return encoded_string
 
-# 画像ファイルのパス
-IMAGE_PATH = "aaa.jpeg" # ここにイメージファイルのパスを設定してください
 
-# 画像をbase64エンコード
-image_base64 = get_base64_of_image('aaa.jpeg')
 
 
 
