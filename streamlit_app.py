4f365fcc7bd616ac38aab32a8ff11e2694c53fd7
tktesting-for-business <taka3chijp@gmail.com>
2025-01-26 09:09:35 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.title("Embedding Dify app in Streamlit")
 
 # 画像ファイルをbase64エンコードしてCSSに埋め込む
 def get_base64_of_image(image_path):
-    encoded_string = base64.b64encode(image_file.read()).decode()
+
     try:
         with open(image_path, "rb") as image_file:
             encoded_string = "test"
