2ed1d5164a3781f8a0c86915fce418302528ccb7
tktesting-for-business <taka3chijp@gmail.com>
2025-01-26 09:07:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.title("Embedding Dify app in Streamlit")
 def get_base64_of_image(image_path):
     try:
         with open(image_path, "rb") as image_file:
-            encoded_string = base64.b64encode(image_file.read()).decode()
+            # encoded_string = base64.b64encode(image_file.read()).decode()
     except FileNotFoundError as e:
         sencoded_string = e
         
